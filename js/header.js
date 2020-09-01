ba58fd227b261433b67eb34cf9c657e4398149cb
inkcube-casimir <70193861+inkcube-casimir@users.noreply.github.com>
2020-09-01 10:31:14 +0800

    Finish Developing Header

---
+++ js/header.js
@@ -0,0 +1,43 @@
+function removeDropdownShow(){
+    dropdown.classList.contains("show") ? dropdown.classList.remove("show") : null;
+}
+
+const bottomHeader = document.querySelector(".page-header .bottom-header");
+const bottomHeaderLinks = bottomHeader.querySelectorAll(".link a");
+const otherBtn = bottomHeader.querySelector("nav button");
+const dropdown = bottomHeader.querySelector("nav .dropdown")
+const searchField = bottomHeader.querySelector("label");
+const searchFieldInput = searchField.querySelector("input");
+const searchFieldPlaceholderText = searchField.querySelector(".placeholder p");
+
+//bottom header links
+for (let i = 0; i < bottomHeaderLinks.length; i++) {
+    const link = bottomHeaderLinks[i];
+    link.addEventListener("mouseover", ()=>{
+        removeDropdownShow();
+
+        link.parentElement.style.borderBottom = "2px solid #e55c61"
+    })
+    link.addEventListener("mouseleave", ()=>{
+        link.parentElement.style.borderBottom = ""
+    })
+}
+
+//other
+otherBtn.addEventListener("mouseover",()=>{
+    dropdown.classList.add("show");
+})
+bottomHeader.addEventListener("mouseleave",()=>{
+    removeDropdownShow();
+})
+searchFieldInput.addEventListener("mouseover", ()=>{
+    removeDropdownShow();
+})
+
+//search field
+searchFieldInput.addEventListener("focus", ()=>{
+    searchFieldPlaceholderText.classList.add("hide");
+})
+searchFieldInput.addEventListener("focusout", ()=>{
+    searchFieldPlaceholderText.classList.remove("hide");
+})
