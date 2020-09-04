e00bf0ec1654fee273cb845513c7bf82bf4664ad
inkcube-casimir <70193861+inkcube-casimir@users.noreply.github.com>
2020-09-04 16:24:30 +0800

    Changes in printer page js

--- js/printerPage.js
+++ js/printerPage.js
@@ -1,4 +1,53 @@
 function init() {
+  //header.js
+  function removeDropdownShow() {
+    dropdown.classList.contains("show")
+      ? dropdown.classList.remove("show")
+      : null;
+  }
+
+  const bottomHeader = document.querySelector(".page-header .bottom-header");
+  const bottomHeaderLinks = bottomHeader.querySelectorAll(".link a");
+  const otherBtn = bottomHeader.querySelector("nav button");
+  const dropdown = bottomHeader.querySelector("nav .dropdown");
+  const searchField = bottomHeader.querySelector("label");
+  const searchFieldInput = searchField.querySelector("input");
+  const searchFieldPlaceholderText = searchField.querySelector(
+    ".placeholder p"
+  );
+
+  //bottom header links
+  for (let i = 0; i < bottomHeaderLinks.length; i++) {
+    const link = bottomHeaderLinks[i];
+    link.addEventListener("mouseover", () => {
+      removeDropdownShow();
+
+      link.parentElement.style.borderBottom = "2px solid #e55c61";
+    });
+    link.addEventListener("mouseleave", () => {
+      link.parentElement.style.borderBottom = "";
+    });
+  }
+
+  //other
+  otherBtn.addEventListener("mouseover", () => {
+    dropdown.classList.add("show");
+  });
+  bottomHeader.addEventListener("mouseleave", () => {
+    removeDropdownShow();
+  });
+  searchFieldInput.addEventListener("mouseover", () => {
+    removeDropdownShow();
+  });
+
+  //search field
+  searchFieldInput.addEventListener("focus", () => {
+    searchFieldPlaceholderText.classList.add("hide");
+  });
+  searchFieldInput.addEventListener("focusout", () => {
+    searchFieldPlaceholderText.classList.remove("hide");
+  });
+
   //finder.js
   const printerSeriesForm = document.querySelector(".finder-form-series");
 
@@ -124,14 +173,14 @@ function init() {
 
       for (let e = 0; e < collapseTrigger.length; e++) {
         const trigger = collapseTrigger[e];
-        
-        if(trigger){
+
+        if (trigger) {
           trigger.addEventListener("click", () => {
             const id = trigger.getAttribute("id");
             const collapseClassList = item.querySelector(
               `.item-bottom .item-collapse-con .${id}`
             ).classList;
-  
+
             if (collapseClassList.contains("collapse-show")) {
               collapseClassList.replace("collapse-show", "collapse-hide");
             } else {
