41952c95dbaf10a063e8f97050d79787de1f3951
inkcube-casimir <70193861+inkcube-casimir@users.noreply.github.com>
2020-09-15 15:55:19 +0800

    Mobile responsive code 1/2

--- js/printerPage.js
+++ js/printerPage.js
@@ -71,17 +71,25 @@ function init() {
   const cardRow = printerFamily[0].getElementsByClassName("card-row");
 
   for (let index = 0; index < cardRow.length; index++) {
-    let collapseState = 0;
+    // let collapseState = 0;
     let collapseCurrent = null;
-
     const card = cardRow[index].getElementsByClassName("card");
 
     for (let e = 0; e < card.length; e++) {
       const button = card[e].getElementsByTagName("button");
       const familySeries = button[0].dataset.familySeries;
-      const collapse = cardRow[0].parentElement.querySelector(
-        `#${familySeries}`
-      );
+      let collapse;
+
+      if(screen.width === 360) {
+        collapse = cardRow[0].parentElement.querySelector(
+          `#${familySeries}-mobile`
+        );
+      } else{
+        collapse = cardRow[0].parentElement.querySelector(
+          `#${familySeries}`
+        );
+      }
+      console.log(collapse)
 
       button[0].addEventListener("click", () => {
         const collapseClassList = collapse.classList;
